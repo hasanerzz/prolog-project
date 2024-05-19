c80dd1e586b98a2042aca9e379711c34957f958d
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-05-19 17:03:46 +0300

    Update deneme.pl

--- deneme.pl
+++ deneme.pl
@@ -1,236 +1,245 @@
-% Define dynamic predicates for family members and marriages
-:- dynamic person/9.
-:- dynamic marriage/2.
+:- dynamic male/1, female/1, parent/2, married/2, birth_year/2, death_year/2.
 
-% Define example base family members
-person(id1, male, 'John', 'Doe', date(1950, 1, 1), date(2020, 12, 31), '', '', []).
-person(id2, female, 'Jane', 'Doe', date(1952, 2, 2), date(2018, 11, 30), '', '', [id3, id4]).
-person(id3, male, 'Jim', 'Doe', date(1970, 3, 3), date(2010, 10, 10), id1, id2, []).
-person(id4, female, 'Emma', 'Doe', date(1980, 4, 4), _, id1, id2, []).
+% Define initial male and female members
+male('Murat Aslan').
+female('Sedanur Aslan').
 
-% Add a new person to the family tree
-add_person(ID, Gender, Name, Surname, BirthDate, DeathDate, Father, Mother) :-
-    \+ person(ID, _, _, _, _, _, _, _, _),  % Ensure the person does not already exist
-    assertz(person(ID, Gender, Name, Surname, BirthDate, DeathDate, Father, Mother, [])),
-    (Father \= '' -> add_child(Father, ID) ; true),
-    (Mother \= '' -> add_child(Mother, ID) ; true).
+% Define initial marriage relationships
+married('Murat Aslan', 'Sedanur Aslan').
 
-% Update birth or death date of a person
-update_person(ID, BirthDate, DeathDate) :-
-    retract(person(ID, Gender, Name, Surname, _, _, Father, Mother, Children)),
-    assertz(person(ID, Gender, Name, Surname, BirthDate, DeathDate, Father, Mother, Children)).
+% Adding a new person
+add_person(Name, male) :-
+    \+ male(Name),
+    assertz(male(Name)).
 
-% Add a child to a parent's children list
-add_child(ParentID, ChildID) :-
-    person(ParentID, Gender, Name, Surname, BirthDate, DeathDate, Father, Mother, Children),
-    retract(person(ParentID, Gender, Name, Surname, BirthDate, DeathDate, Father, Mother, Children)),
-    assertz(person(ParentID, Gender, Name, Surname, BirthDate, DeathDate, Father, Mother, [ChildID | Children])).
+add_person(Name, female) :-
+    \+ female(Name),
+    assertz(female(Name)).
 
-% Check if a person is alive
-is_alive(ID) :-
-    person(ID, _, _, _, _, DeathDate, _, _, _),
-    var(DeathDate).  % DeathDate is unbound if the person is alive
+% Updating a person's gender
+update_gender(Name, male) :-
+    retractall(female(Name)),
+    (male(Name) -> true ; assertz(male(Name))).
 
-% Calculate the age of a person
-calculate_age(ID, Age) :-
-    person(ID, _, _, _, date(BY, BM, BD), DeathDate, _, _, _),
-    get_time(CurrentTime),
-    stamp_date_time(CurrentTime, DateTime, 'UTC'),
-    date_time_value(year, DateTime, CY),
-    date_time_value(month, DateTime, CM),
-    date_time_value(day, DateTime, CD),
-    (   var(DeathDate)  % If the person is alive
-    ->  Age is CY - BY - (CM < BM ; CM == BM, CD < BD)
-    ;   DeathDate = date(DY, DM, DD),
-        Age is DY - BY - (DM < BM ; DM == BM, DD < BD)).
+update_gender(Name, female) :-
+    retractall(male(Name)),
+    (female(Name) -> true ; assertz(female(Name))).
 
-% Calculate the level of a person in the family tree
-calculate_level(ID, Level) :-
-    calculate_level(ID, 0, Level).
-
-calculate_level(ID, Acc, Level) :-
-    person(ID, _, _, _, _, _, Father, _, _),
-    (   Father == '' -> Level is Acc
-    ;   NewAcc is Acc + 1,
-        calculate_level(Father, NewAcc, Level)).
-
-% Find relationships between people
-relationship(Child, Parent, 'Baba') :-
-    person(Child, _, _, _, _, _, Parent, _, _),
-    person(Parent, male, _, _, _, _, _, _, _).
-
-relationship(Child, Parent, 'Anne') :-
-    person(Child, _, _, _, _, _, _, Parent, _),
-    person(Parent, female, _, _, _, _, _, _, _).
-
-relationship(Person1, Person2, 'Erkek Kardeş') :-
-    person(Person1, male, _, _, _, _, Father, Mother, _),
-    person(Person2, _, _, _, _, _, Father, Mother, _),
-    Person1 \= Person2.
-
-relationship(Person1, Person2, 'Kız Kardeş') :-
-    person(Person1, female, _, _, _, _, Father, Mother, _),
-    person(Person2, _, _, _, _, _, Father, Mother, _),
-    Person1 \= Person2.
-
-relationship(Person1, Person2, 'Abla') :-
-    person(Person1, female, _, _, date(Y1, M1, D1), _, Father, Mother, _),
-    person(Person2, _, _, _, date(Y2, M2, D2), _, Father, Mother, _),
-    Person1 \= Person2,
-    (Y1 < Y2 ; Y1 == Y2, M1 < M2 ; Y1 == Y2, M1 == M2, D1 < D2).
-
-relationship(Person1, Person2, 'Abi') :-
-    person(Person1, male, _, _, date(Y1, M1, D1), _, Father, Mother, _),
-    person(Person2, _, _, _, date(Y2, M2, D2), _, Father, Mother, _),
-    Person1 \= Person2,
-    (Y1 < Y2 ; Y1 == Y2, M1 < M2 ; Y1 == Y2, M1 == M2, D1 < D2).
-
-relationship(Person1, Person2, 'Amca') :-
-    person(Person1, _, _, _, _, _, Grandfather, _, _),
-    person(Person2, male, _, _, _, _, Grandfather, _, _),
-    Person1 \= Person2,
-    person(Person1, _, _, _, _, _, Father, _, _),
-    Father \= Person2.
-
-relationship(Person1, Person2, 'Hala') :-
-    person(Person1, _, _, _, _, _, Grandfather, _, _),
-    person(Person2, female, _, _, _, _, Grandfather, _, _),
-    Person1 \= Person2,
-    person(Person1, _, _, _, _, _, Father, _, _),
-    Father \= Person2.
-
-relationship(Person1, Person2, 'Dayı') :-
-    person(Person1, _, _, _, _, _, _, Grandmother, _),
-    person(Person2, male, _, _, _, _, _, Grandmother, _),
-    Person1 \= Person2,
-    person(Person1, _, _, _, _, _, _, Mother, _),
-    Mother \= Person2.
-
-relationship(Person1, Person2, 'Teyze') :-
-    person(Person1, _, _, _, _, _, _, Grandmother, _),
-    person(Person2, female, _, _, _, _, _, Grandmother, _),
-    Person1 \= Person2,
-    person(Person1, _, _, _, _, _, _, Mother, _),
-    Mother \= Person2.
-
-relationship(Person1, Person2, 'Kuzen') :-
-    person(Person1, _, _, _, _, _, Grandfather, _, _),
-    person(Person2, _, _, _, _, _, Grandfather, _, _),
-    Person1 \= Person2,
-    person(Person1, _, _, _, _, _, Father, _, _),
-    Father \= Person2,
-    person(Person2, _, _, _, _, _, _, _, Children2),
-    member(Person1, Children2).
-
-relationship(Person1, Person2, 'Yeğen') :-
-    person(Person1, _, _, _, _, _, _, Parent, _),
-    person(Person2, _, _, _, _, _, _, Parent, _),
-    Person1 \= Person2,
-    (   person(Person2, male, _, _, _, _, _, _, _) -> true
-    ;   person(Person2, female, _, _, _, _, _, _, _)).
-
-relationship(Person1, Person2, 'Enişte') :-
-    marriage(Person2, Person3),
-    relationship(Person1, Person3, 'Kız Kardeş').
-
-relationship(Person1, Person2, 'Yenge') :-
-    marriage(Person2, Person3),
-    relationship(Person1, Person3, 'Erkek Kardeş').
-
-relationship(Person1, Person2, 'Kayınvalide') :-
-    marriage(Person2, Person3),
-    relationship(Person3, Person1, 'Anne').
-
-relationship(Person1, Person2, 'Kayınpeder') :-
-    marriage(Person2, Person3),
-    relationship(Person3, Person1, 'Baba').
-
-relationship(Person1, Person2, 'Gelin') :-
-    marriage(Person2, Person3),
-    person(Person2, female, _, _, _, _, _, _, _),
-    relationship(Person1, Person3, 'Baba').
-
-relationship(Person1, Person2, 'Damat') :-
-    marriage(Person2, Person3),
-    person(Person2, male, _, _, _, _, _, _, _),
-    relationship(Person1, Person3, 'Baba').
-
-relationship(Person1, Person2, 'Bacanak') :-
-    marriage(Person1, Person3),
-    marriage(Person2, Person4),
-    relationship(Person3, Person4, 'Erkek Kardeş').
-
-relationship(Person1, Person2, 'Baldız') :-
-    marriage(Person1, Person3),
-    relationship(Person2, Person3, 'Kız Kardeş').
-
-relationship(Person1, Person2, 'Görümce') :-
-    marriage(Person1, Person3),
-    relationship(Person2, Person3, 'Erkek Kardeş').
-
-relationship(Person1, Person2, 'Kayınbirader') :-
-    marriage(Person1, Person3),
-    relationship(Person2, Person3, 'Erkek Kardeş').
-
-% Marriage validity checks
-valid_marriage(Person1, Person2) :-
-    person(Person1, _, _, _, date(Y1, M1, D1), _, _, _, _),
-    person(Person2, _, _, _, date(Y2, M2, D2), _, _, _, _),
-    get_time(CurrentTime),
-    stamp_date_time(CurrentTime, DateTime, 'UTC'),
-    date_time_value(year, DateTime, CY),
-    date_time_value(month, DateTime, CM),
-    date_time_value(day, DateTime, CD),
-    Age1 is CY - Y1 - (CM < M1 ; CM == M1, CD < D1),
-    Age2 is CY - Y2 - (CM < M2 ; CM == M2, CD < D2),
-    Age1 >= 18,
-    Age2 >= 18,
-    \+ relationship(Person1, Person2, _).
-
-marry(Person1, Person2) :-
-    valid_marriage(Person1, Person2),
-    assertz(marriage(Person1, Person2)).
-
-% Print family tree
+% Printing the family tree
 print_family_tree :-
-    findall(ID, person(ID, _, _, _, _, _, _, _, _), People),
-    print_people(People).
+    findall((Parent, Child), parent(Parent, Child), ParentChildPairs),
+    writeln('Parent-Child Relationships:'),
+    forall(member((Parent, Child), ParentChildPairs), writeln(Parent-Child)),
+    findall((Spouse1, Spouse2), married(Spouse1, Spouse2), MarriedPairs),
+    writeln('Married Couples:'),
+    forall(member((Spouse1, Spouse2), MarriedPairs), writeln(Spouse1-Spouse2)).
+% Calculating age
+current_year(2024).  % This can be dynamically set based on the current year
+calculate_age(Name, Age) :-
+    birth_year(Name, BirthYear),
+    (   death_year(Name, DeathYear) -> Age is DeathYear - BirthYear
+    ;   current_year(CurrentYear), Age is CurrentYear�-�BirthYear).
 
-print_people([]).
-print_people([ID|Rest]) :-
-    person(ID, Gender, Name, Surname, BirthDate, DeathDate, Father, Mother, Children),
-    format('~w ~w ~w, born on ~w, died on ~w~n', [Gender, Name, Surname, BirthDate, DeathDate]),
-    format('Father: ~w, Mother: ~w~n', [Father, Mother]),
+% Printing information about a person
+print_person_info(Name) :-
+    (male(Name) -> Gender = 'Male' ; female(Name) -> Gender = 'Female' ; Gender = 'Unknown'),
+    format('Name: ~w, Gender: ~w~n', [Name, Gender]),
+    findall(Child, parent(Name, Child), Children),
     format('Children: ~w~n', [Children]),
-    print_people(Rest).
+    findall(Parent, parent(Parent, Name), Parents),
+    format('Parents: ~w~n', [Parents]),
+    (married(Name, Spouse) -> format('Spouse: ~w~n', [Spouse]) ; true),
+    (calculate_age(Name, Age) -> format('Age: ~w~n', [Age]) ; writeln('Age:�Unknown')).
 
-% Translate relationship to Turkish
-translate_relationship(Relation, Turkish) :-
-    ( Relation = 'father' -> Turkish = 'Baba'
-    ; Relation = 'mother' -> Turkish = 'Anne'
-    ; Relation = 'brother' -> Turkish = 'Erkek Kardeş'
-    ; Relation = 'sister' -> Turkish = 'Kız Kardeş'
-    ; Relation = 'uncle' -> Turkish = 'Amca'
-    ; Relation = 'aunt' -> Turkish = 'Hala'
-    ; Relation = 'cousin' -> Turkish = 'Kuzen'
-    ; Relation = 'nephew' -> Turkish = 'Yeğen'
-    ; Relation = 'niece' -> Turkish = 'Yeğen'
-    ; Relation = 'husband' -> Turkish = 'Koca'
-    ; Relation = 'wife' -> Turkish = 'Karısı'
-    ; Relation = 'son' -> Turkish = 'Oğul'
-    ; Relation = 'daughter' -> Turkish = 'Kız'
-    ; Relation = 'father-in-law' -> Turkish = 'Kayınpeder'
-    ; Relation = 'mother-in-law' -> Turkish = 'Kayınvalide'
-    ; Relation = 'brother-in-law' -> Turkish = 'Kayınbirader'
-    ; Relation = 'sister-in-law' -> Turkish = 'Baldız'
-    ; Relation = 'grandfather' -> Turkish = 'Dede'
-    ; Relation = 'grandmother' -> Turkish = 'Büyükanne'
-    ; Turkish = Relation).
+% Finding relationships
+find_relationship(X, Y, 'karde�') :- sibling(X, Y).
+find_relationship(X, Y, 'parent') :- parent(X, Y).
+find_relationship(X, Y, 'child') :- parent(Y, X).
+find_relationship(X, Y, 'grandparent') :- grandparent(X, Y).
+find_relationship(X, Y, 'grandchild') :- grandparent(Y, X).
+find_relationship(X, Y, 'amca') :- uncle(X, Y).
+find_relationship(X, Y, 'dayi') :- dayi(X, Y).
+find_relationship(X, Y, 'teyze') :- aunt(X, Y).
+find_relationship(X, Y, 'hala') :- hala(X,Y).
+find_relationship(X, Y, 'cousin') :- cousin(X, Y).
+find_relationship(X, Y, 'spouse') :- married(X, Y).
+find_relationship(X, Y, 'baldiz') :- baldiz(X,Y).
+find_relationship(X, Y, 'kayinbirader') :- kayinbirader(X,Y).
+find_relationship(X, Y, 'elti') :- elti(X,Y).
+find_relationship(X, Y, 'bacanak') :- bacanak(X,Y).
 
-% Example usage
-:- add_person(id5, male, 'Mark', 'Doe', date(2005, 5, 5), _, id3, id4).
-:- update_person(id1, date(1950, 1, 1), date(2021, 1, 1)).
-:- is_alive(id4).
-:- calculate_age(id1, Age), writeln(Age).
-:- relationship(id3, id4, Rel), writeln(Rel).
-:- print_family_tree.
+
+% Additional relationships
+father(F, C) :- male(F), parent(F, C).
+mother(M, C) :- female(M), parent(M, C).
+sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
+grandparent(GP, GC) :- parent(GP, P), parent(P, GC).
+grandfather(GF, GC) :- male(GF), grandparent(GF, GC).
+grandmother(GM, GC) :- female(GM), grandparent(GM, GC).
+uncle(U, N) :- male(U), sibling(U, P),male(P), parent(P, N).
+dayi(D,Y) :-  male(D), sibling(D, P), female(P), parent(P,Y).
+aunt(A, N) :- female(A), sibling(A, P), female(P),parent(P, N).
+hala(H, Y) :- female(H), sibling(H,P), male(P), parent(P,Y).
+cousin(X, Y) :- parent(P1, X), parent(P2, Y), sibling(P1, P2).
+baldiz(S,H) :- male(H),married(H,W), female(S), sibling(S,W).
+kayinbirader(B,H) :- male(B), male(H), married(H,W), sibling(W,B).
+elti(S,W) :- female(S), female(W), married(H,W), married(S,X),
+    sibling(X,H).
+bacanak(S,W) :- male(S), male(W), married(H,W), married(S,X),
+    sibling(X,H).
+eniste(E, H):- male(E), male(H), sibling(H,S), female(S), married(E,S).
+
+
+% Adding a marriage
+add_marriage(Spouse1, Spouse2) :-
+    assertz(married(Spouse1, Spouse2)),
+    assertz(married(Spouse2, Spouse1)).
+
+% Menu
+menu :-
+    repeat,
+    writeln('1-) Ask relation'),
+    writeln('2-) Add/Update person'),
+    writeln('3-) Get information of any person'),
+    writeln('4-) Print the family tree'),
+    writeln('5-) Add marriage'),
+    writeln('6-) Terminate the program'),
+    read(Choice),
+    (   Choice == 1 -> ask_relation, fail
+    ;   Choice == 2 -> add_update_person, fail
+    ;   Choice == 3 -> get_person_info, fail
+    ;   Choice == 4 -> print_family_tree, fail
+    ;   Choice == 5 -> add_marriage_prompt, fail
+    ;   Choice == 6 -> writeln('Terminating program.'), !, fail
+    ;   writeln('Invalid choice, please try again.'), fail
+    ).
+
+% Asking for relationship
+ask_relation :-
+    writeln('Enter the first person:'),
+    read(Person1),
+    writeln('Enter the second person:'),
+    read(Person2),
+    (   find_relationship(Person1, Person2, Relationship) ->
+        format('The relationship between ~w and ~w is: ~w~n', [Person1, Person2, Relationship])
+    ;   writeln('No relationship found.')
+    ).
+
+% Adding or updating a person
+add_update_person :-
+    writeln('Please type the father\'s name:'),
+    read(FatherName),
+    writeln('Please type the mother\'s name:'),
+    read(MotherName),
+    writeln('Please type the child\'s name:'),
+    read(ChildName),
+    writeln('Please type the child\'s gender (male/female):'),
+    read(Gender),
+    writeln('Please type the child\'s birth year:'),
+    read(BirthYear),
+    writeln('Please type the child\'s death year (if deceased, else type "none"):'),
+    read(DeathYearInput),
+    parse_death_year(DeathYearInput, DeathYear),
+    assert_person(FatherName, male),
+    assert_person(MotherName, female),
+    assert_person(ChildName, Gender),
+    assert_parent(FatherName, ChildName),
+    assert_parent(MotherName, ChildName),
+    assert_birth_year(ChildName, BirthYear),
+    assert_death_year(ChildName, DeathYear),
+    writeln('Person added/updated successfully.'),
+    print_family_tree.
+
+parse_death_year('none', none) :- !.
+parse_death_year(Year, Year).
+
+assert_person(Name, Gender) :-
+    (   Gender == male ->
+        assertz(male(Name))
+    ;   Gender == female ->
+        assertz(female(Name))
+    ).
+
+
+assert_parent(ParentName, ChildName) :-
+    assertz(parent(ParentName, ChildName)).
+
+assert_birth_year(Name, Year) :-
+    assertz(birth_year(Name, Year)).
+
+assert_death_year(Name, none) :-
+    !.
+assert_death_year(Name, Year) :-
+    assertz(death_year(Name, Year)).
+
+
+% Getting information about a person
+get_person_info :-
+    writeln('Enter the name of the person:'),
+    read(Name),
+    print_person_info(Name).
+
+
+% Adding a marriage prompt
+add_marriage_prompt :-
+    writeln('Enter the name of the first spouse:'),
+    read(Spouse1),
+    writeln('Enter the name of the second spouse:'),
+    read(Spouse2),
+    ask_relation(Spouse1, Sposue2, Relation1).
+    ask_relation(Spouse2, Spouse1, Relation2).
+    (prohibited_relationship(Spouse1, Spouse2) ->
+        write('INVALID MARRIAGE : ~w - ~w', [Relation1, Relation2)
+    ; underage_marriage(Spouse1, Spouse2) ->
+        writeln('Under 18 age marriage.')
+    ; add_marriage(Spouse1, Spouse2),
+      writeln('Marriage added successfully.')
+����).
+
+underage_marriage(Spouse1, Spouse2) :-
+    (calculate_age(Spouse1, Age1), Age1 < 18);
+    (calculate_age(Spouse2, Age2), Age2 < 18).
+
+prohibited_relationship(Spouse1, Spouse2) :-
+    uncle(Spouse1, Spouse2).
+prohibited_relationship(Spouse1, Spouse2) :-
+    aunt(Spouse1, Spouse2).
+prohibited_relationship(Spouse1, Spouse2) :-
+    parent(Spouse1, Spouse2).
+prohibited_relationship(Spouse1, Spouse2) :-
+    uncle(Spouse2, Spouse1).
+prohibited_relationship(Spouse1, Spouse2) :-
+    aunt(Spouse2, Spouse1).
+prohibited_relationship(Spouse1, Spouse2) :-
+    parent(Spouse2, Spouse1).
+prohibited_relationship(Spouse1, Spouse2) :-
+    dayi(Spouse1, Spouse2).
+prohibited_relationship(Spouse1, Spouse2) :-
+    dayi(Spouse2, Spouse1).
+prohibited_relationship(Spouse1, Spouse2) :-
+    hala(Spouse1, Spouse2).
+prohibited_relationship(Spouse1, Spouse2) :-
+    hala(Spouse2, Spouse1).
+
+
+
+
+
+% Start the program
+:- initialization(menu).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
